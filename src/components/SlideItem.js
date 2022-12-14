028abd3091d5c2d9fe4053c92c7adb0794a8636c
Tin <tinny_nguyen@hotmail.com>
2022-12-14 12:38:57 +0100

    slider

---
+++ src/components/SlideItem.js
@@ -0,0 +1,20 @@
+import parse from "html-react-parser";
+import "../style/news.css";
+
+export default function SlideItem({ post }) {
+    let image = "https://cederdorff.github.io/img/logo512.webp";
+
+    if (post._embedded && post._embedded["wp:featuredmedia"]) {
+        image = post._embedded["wp:featuredmedia"][0].source_url;
+    }
+
+    return (
+        <section className="grid-container">
+        <article>
+            <img src={image} alt={post.title.rendered} />
+            <h2>{parse(post.title.rendered)}</h2>
+            {parse(post.content.rendered)}
+        </article>
+        </section>
+    );
+}
