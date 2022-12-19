6b91f8f2c6bfbc9a74eef8b5a356d1ed314c4fbc
Martin Elsborg <Martin.elsborg@gmail.com>
2022-12-19 13:24:08 +0100

    ksdmkls

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ import heroVideo from '../assets/zappavideo3.mp4'
 const Home = () => {
   return (
     <div name="forside" className='home-container'>
-        <video className='hero-video' src={heroVideo} autoPlay loop muted />
+        <video className='hero-video' src={heroVideo} autoPlay loop muted playsinline />
         <div className='content'>
             <h1 className='hero-text'>Velkommen Til Café Zappa</h1>
         </div>
